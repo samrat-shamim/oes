e46ec17c6387ac5a7526c55a1691087a0584e346
DESKTOP-E57FGGL\samim <bsse0511@iit.du.ac.bd>
2016-10-24 08:52:30 +0600

    create question file upload improved

--- back-end/index.js
+++ back-end/index.js
@@ -35,13 +35,16 @@ mongoose.connection.once('open', function() {
 
 
     var multer = require('multer');
+  var uplaodedFileName;
 
     var storage = multer.diskStorage({ //multers disk storage settings
         destination: function (req, file, cb) {
             cb(null, 'uploads/')
         },
         filename: function (req, file, cb) {
-            var datetimestamp = Date.now();
+          var datetimestamp = Date.now();
+          uplaodedFileName = file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1];
+
             cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
         }
     });
@@ -54,7 +57,7 @@ mongoose.connection.once('open', function() {
                 res.json({error_code:1,err_desc:err});
                 return;
             }
-            res.json({error_code:0,err_desc:null});
+            res.json({error_code:0,err_desc:null, fileName:uplaodedFileName});
         });
     });
     console.log('Listening on port 3000...');
--- front-end/apps/business/question/controllers/create-question.controller.js
+++ front-end/apps/business/question/controllers/create-question.controller.js
@@ -5,9 +5,24 @@
 
             scope.pageTitle = "Create Question";
 
-            scope.uploader = new FileUploader({
-                url: "http://localhost:3000/upload"
-            });
+           var uploader= scope.uploader = new FileUploader({
+                url: "http://localhost:3000/upload",
+                autoUpload: true
+           });
+
+          uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+              var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+              return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+          });
+
+          scope.uploadInprogress = false;
+          scope.uploadingImgFor = null;
+          scope.updateUploadingImgInfo = function(info){
+            scope.uploadingImgFor = info;
+          };
 
             scope.questionModel = {};
 
@@ -91,6 +106,20 @@
                 },
             ]
 
+          function setImgPathToDatabase(res) {
+            if(scope.uploadingImgFor == 'q'){
+              scope.questionModel.titleFigure = res.fileName;
+            }else if(scope.uploadingImgFor == 'oa'){
+              scope.questionModel.optionAFigure = res.fileName;
+            }else if(scope.uploadingImgFor == 'ob'){
+              scope.questionModel.optionBFigure = res.fileName;
+            }else if(scope.uploadingImgFor == 'oc'){
+              scope.questionModel.optionCFigure = res.fileName;
+            }else if(scope.uploadingImgFor == 'od'){
+              scope.questionModel.optionDFigure = res.fileName;
+            }
+          }
+
             scope.createQuestion = function(){
                 var model = {
                     "entityName": "question"
@@ -98,6 +127,47 @@
                 model.entity = scope.questionModel;
                 dataManupulator.manupulate("insert",model);
             }
+
+
+
+          uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+          };
+          uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+          };
+          uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+          };
+          uploader.onBeforeUploadItem = function(item) {
+            scope.uploadInprogress = true;
+            console.info('onBeforeUploadItem', item);
+          };
+          uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+          };
+          uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+          };
+          uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+          };
+          uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+          };
+          uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+          };
+          uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            scope.uploadInprogress = false;
+            setImgPathToDatabase(response);
+            console.info('onCompleteItem', fileItem, response, status, headers);
+          };
+          uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+          };
+
+          console.info('uploader', uploader);
         }]);
 
 
