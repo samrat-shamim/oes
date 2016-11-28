4938c621b369bf60c2b7adb74af205fbe6d70cc8
CyborgSamrat <samratsamimk@yahoo.com>
2016-11-28 09:40:02 +0600

    Backend role based authorization done

--- back-end/middlewares/crudAuthorize.js
+++ back-end/middlewares/crudAuthorize.js
@@ -1,6 +1,7 @@
 var restful = require('node-restful');
 var jwt = require('jsonwebtoken');
 var config = require('../config');
+var fs = require('fs');
 module.exports = function(req, res, next) {
 
     var token = req.body.token || req.query.token || req.headers['authtoken'];
@@ -10,7 +11,15 @@ module.exports = function(req, res, next) {
                 return res.json({ success: false, message: 'Failed to authenticate token.' });
             } else {
                 req.decoded = decoded;
-                next();
+                if(authorizeByRole(req.url,decoded._doc.roles[0], req.body.entityName)){
+                    next();
+                } else{
+                    return res.status(403).send({
+                        success: false,
+                        message: 'Unauthorized request'
+                    });
+                }
+
             }
         });
 
@@ -20,6 +29,36 @@ module.exports = function(req, res, next) {
             message: 'No token provided.'
         });
 
+    }
+
+    var actionPermissionMap = {
+        "/getMany": "read",
+        "/getById": "read",
+        "/insert": "modify",
+        "/update": "modify",
+        "/deleteMany": "modify",
+        "/deleteById": "modify",
+        "/getConnection": "read",
+        "/connect": "modify"
+    }
+    function authorizeByRole(action,role,model){
+        var data;
+         try{
+             data = fs.readFileSync('./configs/models/' + model + '.json');
+         } catch (err){
+             if (err.code === 'ENOENT') {
+                 console.log( model + ".json file not found!");
+             } else {
+                 console.log("Unknown error in "+ model + ".json !");
+             }
+             return false;
+         }
+        data = JSON.parse(data);
+        if(data.permissions[actionPermissionMap[action]].indexOf(role)>=0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 };
